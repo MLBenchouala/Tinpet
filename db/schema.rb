ab7ce3113d5cb4f221f6240b90a7f1cb12e1aa13
Julien COFFIGNOT <juliencoffignot@hotmail.fr>
2022-08-30 16:12:46 +0200

    seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_30_102613) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_30_132507) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_30_102613) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["user_id"], name: "index_pets_on_user_id"
   end
 
