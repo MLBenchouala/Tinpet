c547b55cbdadaa3212162374a95511cb1a974731
Julien COFFIGNOT <juliencoffignot@hotmail.fr>
2022-08-30 16:42:08 +0200

    cloudinary, active-storage, debug seed

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,4 +1,6 @@
 class Pet < ApplicationRecord
   belongs_to :user
-  validates :race, :age, :photos, :sexe, :user_id, :description, presence: true
+  validates :race, :age, :photos, :sexe, :user_id, :description, :name, presence: true
+
+  has_many_attached :photos
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :pets, dependent: :destroy
   has_many :messages, dependent: :destroy
-  has_many :matches, dependent: :destroy
+  # has_many :matches, dependent: :destroy
   has_many :swipes, dependent: :destroy
   validates :name, :gender, presence: true
   # validates :description,  length: { maximum: 400 }
+
+  has_many_attached :photos
 end
