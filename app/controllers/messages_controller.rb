b9c76b028fb00c2894fa8097c30fc2d434566084
Roslan Kaaouachi <roslan.kaaouachi@aol.com>
2022-08-31 14:34:54 +0200

    message

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,7 @@
 class MessagesController < ApplicationController
+  create_table :chatrooms do |t|
+    t.references :first_user, index: true, foreign_key: { to_table: :users }, null: false
+    t.references :second_user, index: true, foreign_key: { to_table: :users }, null: false
+    t.timestamps
+  end
 end
