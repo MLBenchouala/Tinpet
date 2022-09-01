0ffdce07fd1badd304334a0d18732e3ac04a2115
Julien COFFIGNOT <juliencoffignot@hotmail.fr>
2022-09-01 16:50:58 +0200

    view show pet

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  def index
-    @users = policy_scope(User)
-  end
+  # def index
+  #   @users = policy_scope(User)
+  # end
 
   def show
     @user = User.find(params[:id])
