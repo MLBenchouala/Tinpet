9464860a525f786822e9ecddde7e951150237bd1
mohamed larbi benchouala <mohamedlarbi.benchouala@gmail.com>
2022-09-07 10:49:55 +0200

    front

--- app/javascript/controllers/swipe_pets_controller.js
+++ app/javascript/controllers/swipe_pets_controller.js
@@ -41,13 +41,13 @@ export default class extends Controller {
         console.log(data)
         if (data.matched) {
           Swal.fire({
-            title: ` <strong>You matched with ${data.user_2_name} !</strong>`,
             background: `url(${this.backgroundValue})`,
             html:
                `<div class="container">
                   <div class="match--card photo_R"><img src="${data.user_1_photo}" /></div>
                   <div class="match--card photo_L"><img src="${data.user_2_photo}" /></div>
                 </div>
+                <div><h2> It's a match!</h2></div>
                 <div class="my-5"><a href="/matches/${data.match_id}"class="btn">Message them</a></div>
                 <div><a href="/pets"class="btn">Keep swiping</a></div>
                 `
